047c70bb8341169dfa3dc6318fb8aa29e5fd0757
{marlonjt} <{marlonj3@outlook.es}>
2026-03-10 20:31:57 -0500

    feat: autenticación completa con JWT

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,11 @@
+import os
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+from fastapi import HTTPException, status
 
+load_dotenv()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -9,3 +15,28 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido o expirado",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -4,11 +4,12 @@ from sqlalchemy.orm import Session
 # Importaciones locales de mi app
 from app.database import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
-from app.core.security import hash_password
+from app.schemas.user import UserCreate, UserResponse, UserLogin
+from app.core.security import hash_password, create_access_token, verify_password
 
 # Router para el registro
 router = APIRouter(prefix="/auth", tags=["auth"])
+
 
 # Método post para enviar la información del user
 @router.post("/register", response_model=UserResponse, status_code=201)
@@ -31,3 +32,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    # Paso 1: Buscar el usuario por email
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
